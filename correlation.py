0c53b96f2dae07122047ece63508f1073d56f9a4
mmn420 <momen.rizk99@eng-st.cu.edu.eg>
2021-01-16 22:47:35 +0200

    added correlations spreadsheet

--- correlation.py
+++ correlation.py
@@ -1,7 +1,8 @@
 import numpy as np
 from scipy import stats
 import matplotlib.pyplot as plt
-
+import pandas as pd
+#extracting the Gene name and Gene ID from the files
 def extract_names(filename: str):
     nameset = []
     with open(filename) as f:
@@ -26,6 +27,7 @@ def extract_data(filename: str):
 data_healthy=extract_data('lusc-rsem-fpkm-tcga_paired.txt')
 data_cancerous=extract_data('lusc-rsem-fpkm-tcga-t_paired.txt')
 data_names=extract_names('lusc-rsem-fpkm-tcga_paired.txt')
+#Removing the data that has an expression of 0 in more than 50% of the sample
 k = 0
 for i in  data_healthy:
     sum=0
@@ -48,15 +50,18 @@ for i in  data_cancerous:
         data_names.pop(k)
         data_cancerous.pop(k)
     k += 1
-print(len(data_names))
 
-print
 pearson_coeff = []
 spearman_coeff = []
+
 #Getting the pearson coeff for each gene
 [pearson_coeff.append(stats.pearsonr(data_cancerous[i],data_healthy[i])[0]) for i in range(0,len(data_healthy))]
 pearson_coeff_numpy = np.array(pearson_coeff)
-pearson_coeff_numpy[np.isnan(pearson_coeff_numpy)] = 0
+#Export the CC to an excel sheet
+df_names = pd.DataFrame (data_names, columns=['Gene name', 'Gene ID'])
+df_names['Pearson CC'] = pearson_coeff_numpy
+filepath = 'Correlations.xlsx'
+df_names.to_excel(filepath)
 
 #getting spearman coeff
 # [spearman_coeff.append(stats.spearmanr(data_cancerous[i],data_healthy[i])[0]) for i in range(0,len(data_healthy))]
@@ -78,12 +83,14 @@ min_index = np.argmin(pearson_coeff_numpy)
 #Plotting
 fig, (ax1, ax2) = plt.subplots(2)
 fig.suptitle('Gene expression for the highest and lowest correlation coefficient genes.')
-ax1.set_title(f"Gene Maximum Correlation Coefficient{data_names[max_index]}")
-ax2.set_title(f"Gene Minimum Correlation Coefficient{data_names[min_index]}")
-ax1.plot(data_healthy[max_index])
-ax1.plot(data_cancerous[max_index])
-ax2.plot(data_healthy[min_index])
-ax2.plot(data_cancerous[min_index])
-ax1.legend(['data_healthy','data_cancerous'])
-ax2.legend(['data_healthy','data_cancerous'])
+ax1.set_title(f"Gene with Maximum Correlation Coefficient{data_names[max_index]}")
+ax2.set_title(f"Gene with Minimum Correlation Coefficient{data_names[min_index]}")
+ax1.scatter(data_healthy[max_index],data_cancerous[max_index])
+ax1.set(xlabel='data_healthy', ylabel='data_cancerous')
+ax1.plot(np.unique(data_healthy[max_index]), np.poly1d(np.polyfit(data_healthy[max_index], data_cancerous[max_index], 1))(np.unique(data_healthy[max_index])), color='blue')
+ax2.scatter(data_healthy[min_index],data_cancerous[min_index])
+ax2.plot(np.unique(data_healthy[min_index]), np.poly1d(np.polyfit(data_healthy[min_index], data_cancerous[min_index], 1))(np.unique(data_healthy[min_index])), color='blue')
+
+ax1.legend(['relation after fitting', 'data'])
+ax2.legend(['relation after fitting', 'data'])
 plt.show()
